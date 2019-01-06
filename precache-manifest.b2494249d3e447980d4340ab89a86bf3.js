0309ef9dd22ab765c62af9d18ae82eaea09c8787
Brett Jurman <i.be.brett@gmail.com>
2019-01-05 20:57:28 -0500

    Updates

---
+++ precache-manifest.b2494249d3e447980d4340ab89a86bf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "492ab0c1993e79ba655b",
+    "url": "/dd/static/js/runtime~main.492ab0c1.js"
+  },
+  {
+    "revision": "8878df0a68f13429bdb5",
+    "url": "/dd/static/js/main.8878df0a.chunk.js"
+  },
+  {
+    "revision": "773a5977b3434688766e",
+    "url": "/dd/static/js/1.773a5977.chunk.js"
+  },
+  {
+    "revision": "8878df0a68f13429bdb5",
+    "url": "/dd/static/css/main.2fe8ff1f.chunk.css"
+  },
+  {
+    "revision": "4efe1ecda529c58ee70b67efbeb62ffc",
+    "url": "/dd/index.html"
+  }
+];
